4f1f722e57219500ca58cf7701d9a1b1547d7a00
zhouzhounice <894425013@qq.com>
2021-08-29 16:05:19 +0800

    完成品牌管理静态页面布置

--- src/views/product/trademark/list.vue
+++ src/views/product/trademark/list.vue
@@ -1,11 +1,32 @@
 <template lang="">
   <div>
-    List...
+    <el-button type="primary" icon="el-icon-plus">添加</el-button>
+    <el-table border style="width: 100%;margin:20px 0">
+      <el-table-column label="序号" width="80" align="center" />
+      <el-table-column label="品牌名称" width="180" align="center" />
+      <el-table-column label="品牌LOGO" align="center" />
+      <el-table-column label="操作" align="center" />
+    </el-table>
+    <el-pagination
+      style="text-align:center"
+      :total="total"
+      :current-page="page"
+      :page-size="limit"
+      :page-sizes="[4, 6, 8]"
+      layout="prev,pager,next,jumper,->,sizes,total"
+    />
   </div>
 </template>
 <script>
 export default {
-  name: 'TradeMark'
+  name: 'TradeMark',
+  data() {
+    return {
+      page: 1,
+      limit: 3,
+      total: 15
+    }
+  }
 }
 </script>
 <style lang=""></style>
