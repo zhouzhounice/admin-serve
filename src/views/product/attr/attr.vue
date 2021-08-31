4fa80fa06c0b92cbe51743086d3de2bc739b3406
zhouzhounice <894425013@qq.com>
2021-08-31 21:17:32 +0800

    收集数据

--- src/views/product/attr/attr.vue
+++ src/views/product/attr/attr.vue
@@ -6,7 +6,9 @@
     <el-card class="box-card" style="margin:10px 20px">
       <div v-show="isShow">
         <!-- 添加按钮 -->
-        <el-button type="primary" icon="el-icon-plus">添加属性</el-button>
+        <el-button type="primary" icon="el-icon-plus" @click="showAddAttr"
+          >添加属性</el-button
+        >
         <!-- 商品显示列表 -->
         <el-table border style="width: 100%;margin:20px 0" :data="attrList">
           <el-table-column
@@ -32,13 +34,13 @@
             </template>
           </el-table-column>
           <el-table-column label="操作" width="300">
-            <template>
+            <template slot-scope="{ row }">
               <div>
                 <HintButton
                   title="修改属性"
                   type="warning"
                   icon="el-icon-edit"
-                  @click="test"
+                  @click="showAddAttr(row)"
                 />
                 <HintButton
                   title="删除属性"
@@ -60,17 +62,35 @@
           </el-form-item>
         </el-form>
         <!-- 添加按钮 -->
-        <el-button type="primary" icon="el-icon-plus">添加属性值</el-button>
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          :disabled="!attrForm.attrName"
+          @click="addAttrValue"
+          >添加属性值</el-button
+        >
         <!-- 添加表格 -->
-        <el-table style="width: 100%;margin:20px 0" border>
-          <el-table-column prop="date" label="序号" width="80" align="center">
+        <el-table
+          style="width: 100%;margin:20px 0"
+          border
+          :data="attrForm.attrValueList"
+        >
+          <el-table-column type="index" label="序号" width="80" align="center">
           </el-table-column>
           <el-table-column prop="name" label="属性值名称" align="center">
+            <template slot-scope="{ row }">
+              <el-input
+                v-model="row.valueName"
+                placeholder="请输入属性值"
+              ></el-input>
+            </template>
           </el-table-column>
-          <el-table-column prop="address" label="操作"> </el-table-column>
+          <el-table-column prop="address" label="操作">
+            <HintButton title="删除属性" type="danger" icon="el-icon-delete" />
+          </el-table-column>
         </el-table>
         <el-button type="primary">确定</el-button>
-        <el-button plain>取消</el-button>
+        <el-button plain @click="isShow = true">取消</el-button>
       </div>
     </el-card>
   </div>
@@ -84,9 +104,19 @@ export default {
       category2Id: '',
       category3Id: '',
       attrList: [],
-      isShow: false,
+      isShow: true,
       attrForm: {
-        attrName: ''
+        attrName: '',
+        attrValueList: [
+          /* {
+            attrId: 0,
+            // id: 0,
+            valueName: ''
+          } */
+        ],
+        categoryId: 0,
+        categoryLevel: 3
+        // id: 0
       }
     }
   },
@@ -103,8 +133,18 @@ export default {
       )
       this.attrList = data
     },
-    test() {
-      console.log(111)
+    showAddAttr(row) {
+      if (row.id) {
+        this.attrForm = row
+      }
+      this.isShow = false
+    },
+    addAttrValue() {
+      this.attrForm.attrValueList.push({
+        attrId: this.attrForm.id,
+        // id: 0,
+        valueName: ''
+      })
     }
   }
 }
