ccd407b767b82560f8df3e09b7195f6c4863410a
zhouzhounice <894425013@qq.com>
2021-09-01 19:21:02 +0800

    完成了spu列表动态渲染

--- src/api/product/spu.js
+++ src/api/product/spu.js
@@ -1,45 +1,47 @@
 import request from '@/utils/request'
 
-// 获取SPU列表
-export function reqGetSpuList({ page, limit, category3Id }) {
-  return request({
-    url: `/admin/product/${page}/${limit}`,
-    method: 'GET',
-    params: {
-      category3Id
-    }
-  })
-}
+export default {
+  // 获取SPU列表
+  reqGetSpuList({ page, limit, category3Id }) {
+    return request({
+      url: `/admin/product/${page}/${limit}`,
+      method: 'GET',
+      params: {
+        category3Id
+      }
+    })
+  },
 
-// 获取基础销售列表
-export function reqGetBaseSaleAttrList() {
-  return request({
-    url: `/admin/product/baseSaleAttrList`,
-    method: 'GET'
-  })
-}
+  // 获取基础销售列表
+  reqGetBaseSaleAttrList() {
+    return request({
+      url: `/admin/product/baseSaleAttrList`,
+      method: 'GET'
+    })
+  },
 
-// 添加spu
-export function reqSaveSpu(data) {
-  return request({
-    url: `/admin/product/saveSpuInfo`,
-    method: 'POST',
-    data
-  })
-}
+  // 添加spu
+  reqSaveSpu(data) {
+    return request({
+      url: `/admin/product/saveSpuInfo`,
+      method: 'POST',
+      data
+    })
+  },
 
-// 根据spuId获取spu数据
-export function reqGetSpuInfo(spuId) {
-  return request({
-    url: `/admin/product/getSpuById/${spuId}`,
-    method: 'GET'
-  })
-}
+  // 根据spuId获取spu数据
+  reqGetSpuInfo(spuId) {
+    return request({
+      url: `/admin/product/getSpuById/${spuId}`,
+      method: 'GET'
+    })
+  },
 
-// 获取spu对应的图片列表
-export function reqGetSpuImageList(spuId) {
-  return request({
-    url: `/admin/product/spuImageList/${spuId}`,
-    method: 'GET'
-  })
+  // 获取spu对应的图片列表
+  reqGetSpuImageList(spuId) {
+    return request({
+      url: `/admin/product/spuImageList/${spuId}`,
+      method: 'GET'
+    })
+  }
 }
--- src/views/product/spu/spu.vue
+++ src/views/product/spu/spu.vue
@@ -1,28 +1,32 @@
 <template lang="">
   <div>
     <el-card class="box-card" style="margin:0 20px">
-      <CategorySelector></CategorySelector>
+      <CategorySelector
+        @categoryChange="categoryChange"
+        :isShow="isShow"
+      ></CategorySelector>
     </el-card>
     <el-card class="box-card" style="margin:10px 20px">
       <!-- 添加按钮 -->
       <el-button type="primary" icon="el-icon-plus">添加SPU</el-button>
       <!-- 商品显示列表 -->
-      <el-table border style="width: 100%;margin:20px 0">
+      <el-table border style="width: 100%;margin:20px 0" :data="spuList">
         <el-table-column label="序号" width="80" align="center" type="index" />
         <el-table-column
           label="SPU名称"
           width="200"
           align="center"
-          prop="tmName"
+          prop="spuName"
         />
-        <el-table-column label="SPU描述" align="center"> </el-table-column>
+        <el-table-column label="SPU描述" align="center" prop="description">
+        </el-table-column>
         <el-table-column label="操作">
           <template>
             <div>
-              <HintButton title="添加SPU" type="primary" icon="el-icon-plus" />
+              <HintButton title="添加SKU" type="primary" icon="el-icon-plus" />
               <HintButton title="修改SPU" type="warning" icon="el-icon-edit" />
               <HintButton title="查看SKU" type="info" icon="el-icon-view" />
-              <HintButton title="删除SKU" type="danger" icon="el-icon-delete" />
+              <HintButton title="删除SPU" type="danger" icon="el-icon-delete" />
             </div>
           </template>
         </el-table-column>
@@ -47,13 +51,44 @@ export default {
   data() {
     return {
       total: 3,
-      page: 1,
-      limit: 1
+      page: 3,
+      limit: 3,
+      category1Id: '',
+      category2Id: '',
+      category3Id: '',
+      isShow: true,
+      spuList: []
     }
   },
   methods: {
-    handleCurrentChange() {},
-    handleSizeChange() {}
+    // 将拿到的属性id存入父组件中
+    categoryChange({ category1, category2, category3 }) {
+      this.category1Id = category1
+      this.category2Id = category2
+      this.category3Id = category3
+      // 发送请求
+      this.getSpuList()
+    },
+    async getSpuList() {
+      try {
+        const { page, limit, category3Id } = this
+        const {
+          data: { total, records }
+        } = await this.$API.SPU.reqGetSpuList({ page, limit, category3Id })
+        this.total = total
+        this.spuList = records
+      } catch (e) {
+        console.log(e)
+      }
+    },
+    handleCurrentChange(value) {
+      this.page = value
+      this.getSpuList()
+    },
+    handleSizeChange(value) {
+      this.limit = value
+      this.getSpuList()
+    }
   }
 }
 </script>
