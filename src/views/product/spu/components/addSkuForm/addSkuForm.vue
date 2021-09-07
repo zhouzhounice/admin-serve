34e383e679574d7650f42243602421a53c9bab47
zhouzhounice <894425013@qq.com>
2021-09-07 21:01:41 +0800

    改善添加sku静态页面

--- src/views/product/spu/components/addSkuForm/addSkuForm.vue
+++ src/views/product/spu/components/addSkuForm/addSkuForm.vue
@@ -6,27 +6,48 @@
     </el-form-item>
     <!-- SKU名称 -->
     <el-form-item label="SKU名称">
-      <el-input v-model="skuList.skuName"></el-input>
+      <el-input v-model="skuList.skuName" />
     </el-form-item>
     <!-- 价格-->
     <el-form-item label="价格">
-      <el-input v-model="skuList.price"></el-input>
+      <el-input v-model="skuList.price" />
     </el-form-item>
     <!-- 重量 -->
     <el-form-item label="重量">
-      <el-input v-model="skuList.weight"></el-input>
+      <el-input v-model="skuList.weight" />
     </el-form-item>
     <!-- SKU描述 -->
     <el-form-item label="SKU描述">
-      <el-input
-        type="textarea"
-        v-model="skuList.description"
-        rows="4"
-      ></el-input>
+      <el-input v-model="skuList.skuDesc" type="textarea" rows="4" />
     </el-form-item>
     <!-- 平台属性 -->
+    <el-form-item label="平台属性">
+      <el-form ref="form" value="苹果" label-width="80px" :inline="true">
+        <el-form-item label="苹果">
+          <el-select value="" placeholder="请选择">
+            <el-option value="11" />
+            <el-option value="12" />
+          </el-select>
+        </el-form-item>
+      </el-form>
+    </el-form-item>
     <!-- 销售属性 -->
-    <el-form-item label="销售属性"> </el-form-item>
+    <el-form-item label="销售属性">
+      <el-form ref="form" value="苹果" label-width="80px" :inline="true">
+        <el-form-item label="颜色">
+          <el-select value="" placeholder="请选择">
+            <el-option value="11" />
+            <el-option value="12" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="版本">
+          <el-select value="" placeholder="请选择">
+            <el-option value="11" />
+            <el-option value="12" />
+          </el-select>
+        </el-form-item>
+      </el-form>
+    </el-form-item>
     <!-- SPU图片 -->
     <el-form-item label="SPU图片">
       <el-table
@@ -35,12 +56,12 @@
         style="width: 100%"
         @selection-change="handleSelectionChange"
       >
-        <el-table-column type="selection" width="55"> </el-table-column>
-        <el-table-column label="图片" width="120">
+        <el-table-column type="selection" width="55" />
+        <el-table-column label="图片">
           <template slot-scope="scope">{{ scope.row.date }}</template>
         </el-table-column>
-        <el-table-column label="名称" width="120"> </el-table-column>
-        <el-table-column label="操作" show-overflow-tooltip> </el-table-column>
+        <el-table-column label="名称" />
+        <el-table-column label="操作" show-overflow-tooltip />
       </el-table>
     </el-form-item>
     <el-button type="primary">确定</el-button>
@@ -54,48 +75,48 @@ export default {
   data() {
     return {
       skuList: {
-        category3Id: '',
-        createTime: '2021-09-01T11:06:05.802Z',
-        id: '',
-        isSale: 0,
-        price: '',
         skuName: '',
-        skuDefaultImg: '',
-        skuDesc: '',
-        spuId: '',
-        tmId: '',
+        price: '',
         weight: '',
-        skuAttrValueList: [
-          {
-            attrId: 0,
-            attrName: 'string',
-            id: 0,
-            skuId: 0,
-            valueId: 0,
-            valueName: 'string'
-          }
-        ],
-        skuImageList: [
-          {
-            id: 0,
-            imgName: 'string',
-            imgUrl: 'string',
-            isDefault: 'string',
-            skuId: 0,
-            spuImgId: 0
-          }
-        ],
-        skuSaleAttrValueList: [
-          {
-            id: 0,
-            saleAttrId: 0,
-            saleAttrName: 'string',
-            saleAttrValueId: 0,
-            saleAttrValueName: 'string',
-            skuId: 0,
-            spuId: 0
-          }
-        ]
+        skuDesc: ''
+        //   category3Id: '',
+        //   createTime: '2021-09-01T11:06:05.802Z',
+        //   id: '',
+        //   isSale: 0,
+        //   skuDefaultImg: '',
+        //   spuId: '',
+        //   tmId: '',
+        //   skuAttrValueList: [
+        //     {
+        //       attrId: 0,
+        //       attrName: 'string',
+        //       id: 0,
+        //       skuId: 0,
+        //       valueId: 0,
+        //       valueName: 'string'
+        //     }
+        //   ],
+        //   skuImageList: [
+        //     {
+        //       id: 0,
+        //       imgName: 'string',
+        //       imgUrl: 'string',
+        //       isDefault: 'string',
+        //       skuId: 0,
+        //       spuImgId: 0
+        //     }
+        //   ],
+        //   skuSaleAttrValueList: [
+        //     {
+        //       id: 0,
+        //       saleAttrId: 0,
+        //       saleAttrName: 'string',
+        //       saleAttrValueId: 0,
+        //       saleAttrValueName: 'string',
+        //       skuId: 0,
+        //       spuId: 0
+        //     }
+        //   ]
       }
     }
   },
